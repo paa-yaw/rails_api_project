c1e56931f9483aade443503d25e8bb80bf60f483
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-10-25 11:37:47 +0000

    adds create, update, destroy action scoped under user resource

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -31,4 +31,92 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
     it { should respond_with 200 }
   end
 
+  describe "POST #create" do
+  	context "when created successfully" do 
+      before do
+        @user = FactoryGirl.create :user
+        @product_attributes = FactoryGirl.attributes_for :product
+        api_authorization_header @user.auth_token
+        post :create, { user_id: @user.id, product: @product_attributes }
+      end
+
+      it "renders created product in json" do 
+        product_response = json_response
+        expect(product_response[:title]).to eq @product_attributes[:title]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when not created" do 
+      before do
+        @user = FactoryGirl.create :user
+        @invalid_product_attributes = { title: "Smart TV", price: "Twelve Dollars" }
+        api_authorization_header @user.auth_token
+        post :create, { user_id: @user.id, product: @invalid_product_attributes } 
+      end
+
+      it "renders json errors" do
+        product_response = json_response
+        expect(product_response).to have_key(:errors) 
+      end
+
+      it "renders why the error" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number" 
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
+  describe "PUT/PATCH #update" do 
+    before do
+      @user = FactoryGirl.create :user
+      @product = FactoryGirl.create :product, user: @user
+      api_authorization_header @user.auth_token 
+    end
+
+    context "when is updated successfully" do 
+      before do 
+        patch :update, { user_id: @user.id, id: @product.id, product: { title: "An expensive Smart TV" } }
+      end
+
+      it "renders updated product as json" do 
+        product_response = json_response
+        expect(product_response[:title]).to eq "An expensive Smart TV"
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when not updated" do 
+      before do
+        patch :update, { user_id: @user.id, id: @product.id, product: { price: "a hundred dollars" } } 
+      end
+
+      it "renders errors in json" do 
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it "renders reason for errors in json" do 
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
+  describe "DELETE #destroy" do 
+    before do
+      @user = FactoryGirl.create :user
+      @product = FactoryGirl.create :product, user: @user
+      api_authorization_header @user.auth_token 
+      delete :destroy, { user_id: @user.id, id: @product.id }
+    end
+
+    it { should respond_with 204 }
+  end
 end
